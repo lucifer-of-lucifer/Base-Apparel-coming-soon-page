bcbe0b97dd4790ed6670deb5e6ad2f7a29125359
Md Jahid Hasan <64312518+lucifer-of-lucifer@users.noreply.github.com>
2020-04-27 17:28:41 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form'),
+      email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const emailValue = email.value;
+
+    if(!validateEmail(emailValue)) {
+        form.classList.add('error');
+    }else {
+        form.classList.remove('error');
+        document.body.innerHTML = '<h3>Thak You!</h3>';
+    }
+
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
